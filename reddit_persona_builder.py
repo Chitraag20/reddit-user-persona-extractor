aa91f3c17b4999432660e4a53df3ad3c5b5f0ea0
Chitraag20 <chitraagrawal2008@gmail.com>
2025-07-16 12:29:18 +0530

    Update reddit_persona_builder.py

--- reddit_persona_builder.py
+++ reddit_persona_builder.py
@@ -12,7 +12,7 @@ def get_reddit_client():
     return praw.Reddit(
         client_id=os.getenv("REDDIT_CLIENT_ID"),
         client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-        user_agent=os.getenv("REDDIT_USER_AGENT")
+        user_agent=os.getenv("REDDIT_USER_AGENT"),
         github_token = os.getenv("TOKEN")
     )
 
